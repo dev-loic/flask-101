53b014e4aa3e55d5d8941e70a11bd0e541ba4d4e
loic.saillant <loic.saillant.dev@gmail.com>
2021-12-20 13:18:31 +0100

    use jsonify to return an array

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,14 @@
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Le Wagon'},
 }
 
 @app.route('/api/v1/produits')
-def hello():
-    return PRODUCTS
+def retrieveProducts():
+    return jsonify(list(PRODUCTS.values()))
