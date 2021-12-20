e391fa224a90f1280afdeac1202894be9c440a8c
loic.saillant <loic.saillant.dev@gmail.com>
2021-12-20 18:55:56 +0100

    Add DELETE route

--- wsgi.py
+++ wsgi.py
@@ -14,7 +14,7 @@ def retrieveProducts():
     return jsonify(list(PRODUCTS.values()))
 
 # READ
-@app.route('/api/v1/products/<int:product_id>')
+@app.route('/api/v1/products/<int:product_id>', methods=['GET'])
 def retrieveProduct(product_id):
     products = list(PRODUCTS.values())
     filtered_products = [product for product in products if product['id'] == product_id]
@@ -22,3 +22,12 @@ def retrieveProduct(product_id):
         abort(404)
     else:
         return filtered_products[0]
+    
+# DELETE
+@app.route('/api/v1/products/<int:product_id>', methods=['DELETE'])
+def deleteProduct(product_id):
+    element_to_delete = PRODUCTS.pop(product_id, None)
+    if element_to_delete is None:
+        abort(404)
+    else:
+        return ('', 204)
