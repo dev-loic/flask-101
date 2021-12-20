081df19092300ed67e72aca017240804592232ff
loic.saillant <loic.saillant.dev@gmail.com>
2021-12-20 13:18:45 +0100

    Implement first tests

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_read_many_products(self):
+        response = self.client.get("/api/v1/produits")
+        products = response.json
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 2)
